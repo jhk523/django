443dc709e02a78134bd21573efed1294cd017d59
junghoonkim <rlawjdgns4345@gmail.com>
2020-08-05 01:48:15 +0900

    test

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,6 +14,8 @@ import os
 import sys
 import json
 
+test = ""
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
